66446165431cfac7b24c81197db1eca93da254c0
hsnakd <hsnakd10@gmail.com>
2024-07-29 19:34:30 +0300

    day04 completed

--- tests/day04/04_file-upload-download.test.js
+++ tests/day04/04_file-upload-download.test.js
@@ -1,6 +1,8 @@
 
 import { test, expect } from "@playwright/test";
 import path from "path";
+const fs = require('fs');
+
 
 //create a test group with 3 tests in it
 test.describe("File upload and download", async () => {
@@ -14,6 +16,30 @@ test.describe("File upload and download", async () => {
   });
 
   test("File upload test", async ({ page }) => {
+    // Create a download directory if it doesn't exist
+    const uploadDir = path.join(__dirname, 'upload');
+    if (!fs.existsSync(uploadDir)) {
+      fs.mkdirSync(uploadDir);
+    }
+
+    // // Create a "Upload File.txt" file in the upload directory if it doesn't exist
+    // const uploadFilePath = path.join(uploadDir, 'Upload File.txt');
+    // if (!fs.existsSync(uploadFilePath)) {
+    //   fs.writeFileSync(uploadFilePath, ''); // Create an empty file or add content here
+    // }
+
+
+    // Create a "Upload File.txt" file in the upload directory if it doesn't exist
+    const uploadFilePath = path.join(uploadDir, 'Upload File.txt');
+    if (!fs.existsSync(uploadFilePath)) {
+      // Create an empty file or add content here
+      fs.writeFileSync(uploadFilePath, 'This file generated automatically1');
+    } else {
+      // Update the file with the specified content
+      // fs.writeFileSync(uploadFilePath, 'This file generated automatically2');
+    }
+
+
     await page.click("text='File Upload'");
 
     expect(page.url()).toBe("https://practice.cydeo.com/upload");
@@ -37,20 +63,44 @@ test.describe("File upload and download", async () => {
 
   test("File Download Test", async ({ page }) => {
     // setting listener for download event
-    await page.click("text='File Download'");
+    const downloadPromise = page.waitForEvent("download"); // promise is created: pending promise
 
+    await page.click("text='File Download'");
     expect(page.url()).toBe("https://practice.cydeo.com/download");
+
+    page.click("//a[@href='download/Sesson3.txt']");
+
+    const download = await downloadPromise; // promise is either full filled or rejected
+    expect(download.suggestedFilename()).toBe("Sesson3.txt");
 
   });
 
   
   
   test("Save the file that's downloaded", async ({ page }) => {
+    // Create a download directory if it doesn't exist
+    const downloadDir = path.join(__dirname, 'download');
+    if (!fs.existsSync(downloadDir)) {
+      fs.mkdirSync(downloadDir);
+    }
 
-  
+    // setting listener for download event
+    const downloadPromise = page.waitForEvent("download"); // promise is created: pending promise
 
+    await page.click("text='File Download'");
+    expect(page.url()).toBe("https://practice.cydeo.com/download");
 
+    page.click("//a[@href='download/Sesson3.txt']");
+
+    const download = await downloadPromise; // promise is either full filled or rejected
+    expect(download.suggestedFilename()).toBe("Sesson3.txt");
+
+    // save the file to the downloads folder
+    // if the file already exists then it will be overwritten with the new file
+    const downloadPath = path.join(__dirname, "download", download.suggestedFilename());
+    await download.saveAs(downloadPath);
   });
 
 
+  
 });
